2c29b4c474e23ce12d6a55f89cde3f6273439d64
Tushar Chhabra <tusharhchhabra@gmail.com>
2023-09-06 00:12:13 -0700

    Added partials to templates

--- express_server.js
+++ express_server.js
@@ -21,6 +21,22 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:id", (req, res) => {
+  const id = req.params.id;
+  const templateVars = { id: id, longURL: urlDatabase[id] };
+  res.render("urls_show", templateVars);
+});
+
+app.get("/u/:id", (req, res) => {
+  const id = req.params.id;
+  res.redirect(urlDatabase[id]);
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
