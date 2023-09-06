da24ed90f54f23b47ff55d893cafbb32cb6066dc
Tushar Chhabra <tusharhchhabra@gmail.com>
2023-09-06 15:11:52 -0700

    Added cookie-parser

--- express_server.js
+++ express_server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
+const cookieParser = require('cookie-parser');
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -12,12 +14,9 @@ const urlDatabase = {
 
 const usersDatabase = {};
 
+
 app.get("/", (req, res) => {
   res.redirect("/urls");
-});
-
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
 });
 
 
@@ -35,9 +34,13 @@ app.post("/login", (req, res) => {
 });
 
 
+
 // URL Database
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = {
+    username: req.cookies["username"],
+    urls: urlDatabase
+  };
   res.render("urls_index", templateVars);
 });
 
@@ -45,7 +48,6 @@ app.post("/urls", (req, res) => {
   const longURL = req.body["longURL"];
   const shortURL = generateRandomString();
   urlDatabase[shortURL] = longURL;
-  const templateVars = { id: shortURL, longURL: urlDatabase[shortURL] };
   res.redirect(`/urls/${shortURL}`);
 });
 
@@ -56,35 +58,48 @@ app.get("/urls/new", (req, res) => {
 });
 
 
-// URL Show Detail
+
+// CRUD
+
+// Read
 app.get("/urls/:id", (req, res) => {
   const id = req.params.id;
-  const templateVars = { id: id, longURL: urlDatabase[id] };
+  const templateVars = { 
+    username: req.cookies["username"],
+    id: id, 
+    longURL: urlDatabase[id] 
+  };
   res.render("urls_show", templateVars);
 });
 
+//Create, Update
 app.post("/urls/:id", (req, res) => {
   const id = req.params.id;
   const longURL = req.body.longURL;
   urlDatabase[id] = longURL;
-  const templateVars = { id: id, longURL: urlDatabase[id] };
   res.redirect(`/urls`);
 });
 
+// Delete
 app.post("/urls/:id/delete", (req, res) => {
   const id = req.params.id;
   delete urlDatabase[id];
   res.redirect("/urls");
 });
 
+// Open Long URL
 app.get("/u/:id", (req, res) => {
   const id = req.params.id;
   res.redirect(urlDatabase[id]);
 });
 
+
+
+// Listen
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
 
 
 // Generate random string
